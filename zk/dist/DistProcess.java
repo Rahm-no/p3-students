df1ce616973ae3375cb645710154f3b267cb2f60
clu36 <clu36@mimi.cs.mcgill.ca>
2023-12-02 22:42:24 -0500

    doing q2

--- zk/dist/DistProcess.java
+++ zk/dist/DistProcess.java
@@ -47,64 +47,127 @@ public class DistProcess implements Watcher
 		System.out.println("DISTAPP : Process information : " + pinfo);
 	}
 
+    // ------------------------ overriding async callbacks ------------------------------------
+
+    AsyncCallback.StringCallback createCallback = new AsyncCallback.StringCallback() {
+        @Override
+        public void processResult(int rc, String path, Object ctx, String name) {
+            // This method gets called once create operation completes
+            // rc is the result code of the operation
+            // path is the path for which this request was issued
+            // ctx is the context object passed when the method was called
+            // name is the actual path that was created
+            String context = (String) ctx; 
+            if (context.equals("createManager")) {
+                System.out.println("create() processResult: Manager");
+            } else if (context.equals("createWorker")) {
+                System.out.println("create() processResult: Workers");
+            } else if (context.equals("createInitialAssignments")) {
+                System.out.println("create() processResult: Assignments");
+            } else if (context.equals("createAssignment")) {
+                System.out.println("create() processResult: Assignments");
+            }
+            System.out.println("Create znode status: " + KeeperException.Code.get(rc) + ", path: " + path);
+        }
+    };
+
+    AsyncCallback.Children2Callback getChildren2Callback = new AsyncCallback.Children2Callback() {
+        @Override
+        public void processResult(int rc, String path, Object ctx, List<String> children, Stat stat) {
+            // This method gets called once the operation completes
+            // rc is the result code of the operation
+            // path is the path for which this request was issued
+            // ctx is the context object passed when the method was called
+            // children is the list of children of the znode
+            // stat is the Stat object containing metadata about the znode
+    
+            if (path.equals("/dist30/tasks")) {
+                System.out.println("getChildren processResult: tasks"); 
+            } else if (path.equals("/dist30/workers")) {
+                System.out.println("getChildren processResult: workers"); 
+            }
+    
+            // Optionally, you can use the Stat object to get additional information about the znode
+            // For example:
+            // System.out.println("Znode version: " + stat.getVersion());
+        }
+    };
+    
+    // ------------------------ initializing ------------------------
+
 	void startProcess() throws IOException, UnknownHostException, KeeperException, InterruptedException
 	{
 		zk = new ZooKeeper(zkServer, 10000, this); //connect to ZK.
 	}
 
-	void initalize()
+	void initalize() throws UnknownHostException, KeeperException, InterruptedException
 	{
+        System.out.println("DISTAPP : initialize()");
 		try
-		{
-			runForManager();	// See if you can become the manager (i.e, no other manager exists)
-			isManager=true;
-			getTasks(); // Install monitoring on any new tasks that will be created.
-									// TODO monitor for worker tasks?
-		} catch(NodeExistsException nee) { 
-            isManager=false; 
-            try {
-                becomeWorker(); 
-            } catch(UnknownHostException uhe)
-            { System.out.println(uhe); }
-            catch(KeeperException ke)
-            { System.out.println(ke); }
-            catch(InterruptedException ie)
-            { System.out.println(ie); }
-        } // TODO: What else will you need if this was a worker process?
-		catch(UnknownHostException uhe)
-		{ System.out.println(uhe); }
-		catch(KeeperException ke)
-		{ System.out.println(ke); }
-		catch(InterruptedException ie)
-		{ System.out.println(ie); }
-
+            {
+                runForManager();	// See if you can become the manager (i.e, no other manager exists)
+                isManager=true;
+                getTasks(); // Install monitoring on any new tasks that will be created.
+                // TODO monitor for worker tasks?
+                getWorkers();
+                createInitialAssignments(); 
+            } catch(NodeExistsException nee) { 
+                // TODO: What else will you need if this was a worker process?
+                isManager=false; 
+                createWorker(); 
+            }
 		System.out.println("DISTAPP : Role : " + " I will be functioning as " +(isManager?"manager":"worker"));
-
 	}
 
+    // ------------------------ helper funcs ------------------------
 	// Manager fetching task znodes...
 	void getTasks()
 	{
-		zk.getChildren("/dist30/tasks", this, this, null);  
+        System.out.println("DISTAPP : getTasks()");
+		zk.getChildren("/dist30/tasks", this, getChildren2Callback, "getTasks()");  
 	}
+    
+    void getWorkers()
+	{
+        System.out.println("DISTAPP : getWorkers()");
+		zk.getChildren("/dist30/workers", this, getChildren2Callback, "getWorkers()");  
+	}
+
+    void getAssignments() {
+        System.out.println("DISTAPP : getAssignments()");
+		zk.getChildren("/dist30/assignments", this, getChildren2Callback, "getAssignments()");  
+    }
 
 	// Try to become the manager.
 	void runForManager() throws UnknownHostException, KeeperException, InterruptedException
 	{
 		//Try to create an ephemeral node to be the manager, put the hostname and pid of this process as the data.
 		// This is an example of Synchronous API invocation as the function waits for the execution and no callback is involved..
-		zk.create("/dist30/manager", pinfo.getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+        System.out.println("DISTAPP : runForManager()");
+		zk.create("/dist30/manager", pinfo.getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL, createCallback, "createManager");
 	}
 
     // become the worker.
-	void becomeWorker() throws UnknownHostException, KeeperException, InterruptedException
+	void createWorker() throws UnknownHostException, KeeperException, InterruptedException
 	{
 		//Try to create an ephemeral node to be a worker, put the hostname and pid of this process as the data.
 		// This is an example of Synchronous API invocation as the function waits for the execution and no callback is involved..
-		zk.create("/dist30/worker/worker-" + new String(pinfo.getBytes()), pinfo.getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+        System.out.println("DISTAPP : becomeWorker()");
+		zk.create("/dist30/worker/worker-" + new String(pinfo.getBytes()), pinfo.getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL, createCallback, "createWorker");
 	}
 
-	public void process(WatchedEvent e)
+    void createInitialAssignments() throws UnknownHostException, KeeperException, InterruptedException {
+        System.out.println("DISTAPP : createInitialAssignments()");
+		zk.create("/dist30/assignments" + new String(pinfo.getBytes()), pinfo.getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL, createCallback, "createInitialAssignments");
+    }
+
+    void createAssignments() throws UnknownHostException, KeeperException, InterruptedException {
+        System.out.println("DISTAPP : createAssignments()");
+        zk.create("/dist30/assignments/worker-" + new String(pinfo.getBytes()), pinfo.getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL, createCallback, "createAssignment"); 
+    }
+
+
+	public void process(WatchedEvent e) 
 	{
 		//Get watcher notifications.
 
@@ -121,22 +184,39 @@ public class DistProcess implements Watcher
 		if(e.getType() == Watcher.Event.EventType.None) // This seems to be the event type associated with connections.
 		{
 			// Once we are connected, do our intialization stuff.
+            System.out.println("DISTAPP : Event received : if statement 1 :" + e);
 			if(e.getPath() == null && e.getState() ==  Watcher.Event.KeeperState.SyncConnected && initalized == false) 
 			{
-				initalize();
-				initalized = true;
+                try {
+                    initalize();
+                    initalized = true;
+                } catch (UnknownHostException exception) {
+                } catch (KeeperException exception) {
+                } catch (InterruptedException exception) {
+                }
 			}
 		}
 
 		// Manager should be notified if any new znodes are added to tasks.
-		if(e.getType() == Watcher.Event.EventType.NodeChildrenChanged && e.getPath().equals("dist30/tasks"))
+		if(e.getType() == Watcher.Event.EventType.NodeChildrenChanged && e.getPath().equals("/dist30/tasks"))
 		{
 			// There has been changes to the children of the node.
 			// We are going to re-install the Watch as well as request for the list of the children.
+            System.out.println("DISTAPP : Event received : if statement 2 :" + e);
 			getTasks();
-		}
+		} if (e.getType() == Watcher.Event.EventType.NodeChildrenChanged && e.getPath().equals("/dist30/watchers")) {
+            System.out.println("DISTAPP : Event received : if statement 3 :" + e);
+            getWorkers();
+            try {
+                createAssignments(); 
+            } catch (UnknownHostException exception) {
+            } catch (KeeperException exception) {
+            } catch (InterruptedException exception) {
+            }
+        }
 	}
 
+    // DEAD FUNCTION currently not used 
 	//Asynchronous callback that is invoked by the zk.getChildren request.
 	public void processResult(int rc, String path, Object ctx, List<String> children)
 	{
@@ -154,8 +234,8 @@ public class DistProcess implements Watcher
 
 		// TODO: Filter out and go over only the newly created task znodes.
 		//		Also have a mechanism to assign these tasks to a "Worker" process.
-		//		The worker must invoke the "compute" function of the Task send by the client.
-		//What to do if you do not have a free worker process?
+		//		The worker must invoke the "compute" function of the Task sent by the client.
+		//      What to do if you do not have a free worker process?
 		System.out.println("DISTAPP : processResult : " + rc + ":" + path + ":" + ctx);
 		for(String c: children)
 		{
@@ -195,14 +275,27 @@ public class DistProcess implements Watcher
 		}
 	}
 
-	public static void main(String args[]) throws Exception
-	{
-		//Create a new process
-		//Read the ZooKeeper ensemble information from the environment variable.
-		DistProcess dt = new DistProcess(System.getenv("ZKSERVER"));
-		dt.startProcess();
-
-		//Replace this with an approach that will make sure that the process is up and running forever.
-		Thread.sleep(20000); 
-	}
+    public static void main(String args[]) throws Exception {
+        // Create a new process
+        // Read the ZooKeeper ensemble information from the environment variable.
+        DistProcess dt = new DistProcess(System.getenv("ZKSERVER"));
+        dt.startProcess();
+    
+        // Loop to keep the main thread running indefinitely
+        while (true) {
+            try {
+                // Sleep for a specified amount of time (e.g., 5 seconds)
+                Thread.sleep(5000);
+    
+                // You can add any necessary checks or operations inside this loop
+                // For example, checking if the process should still be running, or performing periodic tasks
+    
+            } catch (InterruptedException e) {
+                // Optional: Handle the InterruptedException
+                System.out.println("Main thread interrupted, exiting.");
+                break;
+            }
+        }
+    }
+    
 }
